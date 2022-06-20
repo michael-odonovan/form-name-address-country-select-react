ecf5fdfb91e20b1ddd38733ef90bbe7799175467
michael o'donovan <mikeodonovan3@gmail.com>
2022-06-20 16:32:16 +0100

    feat: add basic html elements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import './App.css';
 
+// note that input elements in React have to be self closing tags
+
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <form>
+        <div>
+          <label htmlFor="email">email:</label>
+          <input type="text"/>
+        </div>
+        <div>
+          <label htmlFor="name">name:</label>
+          <input type="text"/>
+        </div>
+        <div>
+          <label htmlFor="address:">address:</label>
+          <input type="text"/>
+        </div>
+
+        <div>
+          <label htmlFor="country">country:</label>
+          <select id="country" name="country">
+            <option value="Australia">Australia</option>
+            <option value="Japan">Japan</option>
+            <option value="England">England</option>
+          </select>
+        </div>
+
+        <input type="submit" value="submit form"/>
+      </form>
+    </div>
   );
 }
 
